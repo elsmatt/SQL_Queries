e5694165bb1930b9dbf28db387dc274075c572f5
elsmatt <90786932+elsmatt@users.noreply.github.com>
2024-04-23 16:29:41 -0400

    Update art_musuem.sql

--- General_Queries/art_musuem/art_musuem.sql
+++ General_Queries/art_musuem/art_musuem.sql
@@ -1,17 +1,17 @@
-SELECT ar.`id` 					AS artist_id,
-    ar.`art_museum_id` 				AS art_museum_id,
-    ar.`artist_name`        			AS artist_name,
-    ar.`art_category` 				AS art_category,
-    ar.`country_name` 				AS country_name,
-    c.`collection_date` 			AS art_collection_date,
+SELECT ar.`id` 							AS artist_id,
+	ar.`art_museum_id` 					AS art_museum_id,
+    ar.`name`        			        AS artist_name,
+    ar.`art_category` 					AS art_category,
+    ar.`country_name` 					AS country_name,
+	c.`collection_date` 				AS art_collection_date,
     c.`pottery_count           `		AS pottery_count,
-    p.`purchase_year` 				AS purchase_year,
-    p.`purchase_month `     			AS purchase_month,
-    s.`unselected` 				AS unselected,
-    s.`selected`         			AS selected,
-    s.`selection_type_id`              		AS selection_type_id,
-    s.`future_selected` 			AS future_selected,
-    s.`future_unselected` 			AS future_unselected
+	p.`purchase_year` 					AS purchase_year,
+	p.`purchase_month `     			AS purchase_month,
+	s.`unselected` 						AS unselected,
+	s.`selected`         				AS selected,
+    s.`selection_type_id`               AS selection_type_id,
+	s.`future_selected` 				AS future_selected,
+	s.`future_unselected` 				AS future_unselected
 FROM artists AS ar
 LEFT JOIN collections AS c ON c.`id` = ar.`collection_id`
 LEFT JOIN purchases AS p ON p.`id` = c.`collection_purchase_id`
